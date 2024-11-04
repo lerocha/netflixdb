e2187fe9e57a7058b31b37bc7176cdc328323b3d
Luis Rocha <lerocha@gmail.com>
2024-11-03 19:56:54 -0800

    chore: updating script to generate the schema

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 ./gradlew clean build
-for p in postgres mysql oracle; do java -jar -Dspring.batch.job.enabled=false -Dspring.profiles.active=$p build/libs/netflixdb-0.0.1-SNAPSHOT.jar; done
+for p in postgres mysql oracle; do java -jar -Dspring.batch.job.enabled=false -Dspring.profiles.active=$p -Dspring.jpa.properties.javax.persistence.schema-generation.scripts.action=create build/libs/netflixdb-0.0.1-SNAPSHOT.jar; done
