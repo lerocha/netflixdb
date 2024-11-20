298730d19ca6274d4fbad077cba212e074cdb45f
Luis Rocha <lerocha@gmail.com>
2024-11-20 08:51:36 -0800

    chore: disabling context load test

--- src/test/kotlin/com/github/lerocha/netflixdb/NetflixDbApplicationTests.kt
+++ src/test/kotlin/com/github/lerocha/netflixdb/NetflixDbApplicationTests.kt
@@ -2,7 +2,9 @@ package com.github.lerocha.netflixdb
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Ignore
 
+@Ignore
 @SpringBootTest
 class NetflixDbApplicationTests {
     @Test
